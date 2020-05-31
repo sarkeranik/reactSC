f6c753ad13c2c60d1706d7f1f2876a8ba05ae851
sarkeranik <anikchandrasarker77@gmail.com>
2020-06-01 00:02:16 +0600

    React Component Part 1

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import React from "react";
 import logo from "./logo.svg";
 import { Navbar, NavbarBrand } from "reactstrap";
+import Menu from './components/MenuComponents';
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <Navbar dark color="primary">
         <div classNam="container">
           <NavbarBrand href="/">Restorent Confusion</NavbarBrand>
         </div>
       </Navbar>
+      <Menu />
     </div>
+
   );
 }
 
